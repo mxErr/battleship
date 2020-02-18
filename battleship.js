2bd0e7bfebe8d2648771dd08356bb24143c6947d
mxErr <lis.mxr@gmail.com>
2020-02-18 08:41:57 +0300

    v1.1 Version with array

--- battleship.js
+++ battleship.js
@@ -2,6 +2,8 @@ let loc,
     location1 = Math.floor(Math.random() * 5),
     location2 = location1 + 1,
     location3 = location2 + 1,
+    locations = [location1, location2, location3],
+    locationsHits = [],
     guess,
     hits = 0,
     guesses = 0,
@@ -9,36 +11,35 @@ let loc,
     misses = 0;
 
 while (isSunk == false) {
-    guess = prompt('Введите число от 0 до 6');
+    guess = +prompt('Введите число от 0 до 6');
     console.log(location1, location2, location3)
-    if (guess < 0 || guess > 6 || isNaN(guess) || guess == null) {
+    if (guess < 0 || guess > 6 || isNaN(guess) || guess == null || guess == '') {
         alert('введите кооректные данные');
     }else{
         guesses++;
-        
-        if (guess == location1 || guess == location2 || guess == location3){
+        if (locationsHits.indexOf(guess) !== -1) {
+            alert('Вы сюда уже стреляли');
+        } 
+        if(locations.indexOf(guess) != -1 ) {
+            alert('Попал');
             hits++;
-            if (guess == location1) {
-                location1 = false;
+            for (let i = 0; i < locations.length; i++) {
+                if(locations[i] == guess) {
+                    locationsHits.push(guess);
+                    delete locations[i];
+                } 
             }
-            if (guess == location2) {
-                location2 = false;
-            }
-            if (guess == location3) {
-                location3 = false;
-            }
-            
-            if (hits == 3) {
+            if(hits == 3) {
+                alert('Корабль потоплен');
                 isSunk = true;
-                alert ('Вы потопили корабль');
             }
         }
         else{
             alert("Miss");
-            console.log(guess)
+            console.log(guess);
             misses++;
         }
     }
 }
-let stats = "Вы сделали " + guesses + " выстрелов." + 3/guesses*100 + " точность. " + misses + " промахов";
-alert(stats)
+let stats = "Вы сделали " + guesses + " выстрелов." + Math.round(3/guesses*100) + '%' + " точность. " + misses + " промахов";
+alert(stats);
